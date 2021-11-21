acbff389f04a2580b249498fb36242d730ea5408
aheldes <59527963+aheldes@users.noreply.github.com>
2021-11-21 22:36:10 +0100

    push

--- data_preparation.py
+++ data_preparation.py
@@ -1,9 +1,12 @@
 import numpy as np
+import pandas as pd
 
 columns_to_boolean = ['Saving_Amount', 'Current_Amount', 'Time_Deposits_Amount', 'Funds_Amount',
                             'Stocks_Amount', 'Bank_Assurance_Amount', 'Life_Assurance_Amount', 'Business_Loan_Amount', 
                             'Home_Loan_Amount', 'Consumer_Loan_Amount', 'Branch_Transactions', 'ATM_Transactions',
                             'Phone_Transactions', 'Internet_Transactions', 'Standing_Orders']
+
+columns = ["Age", "Year_Of_Account_Creation"]
 
 def modifyData(df):
 
@@ -17,18 +20,14 @@ def modifyData(df):
 
     df['Number_Of_Used_Services'] = df[[f"{column}_Flag" for column in columns_to_boolean if f"{column}_Flag".endswith("Amount_Flag") == True]].sum(axis=1)
 
-    return df
-
-def addAgeCategory(df):
-    intervals = [(0, 22),(22, 27), (27, 33), (33, 43), (43, 56)]
-
-    for name in ["Age", "Year_Of_Account_Creation"]:
-        for interval in intervals:
-            min, max = interval[0], interval[1]
-            df[f"{name} - {min}-{max}"] = np.where((df[name] <= max) & (df[name] > min), 1, 0)
-
+    for name in columns:
         conditions = [df[name] <= 18, df[name] <= 27, df[name] <= 33, df[name] <= 43, df[name] <= 56]
         choices = ["18-22","22-27","27-33","33-43","43-56"]
         df[f"{name}_Category"] = np.select(conditions, choices, default="65+")
+
+    return df
+
+def changeToDummies(df):  
+    df = pd.get_dummies(df, columns=[f"{name}_Category" for name in columns])
         
     return df
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import local
-from data_preparation import modifyData, addAgeCategory, columns_to_boolean
+from numpy.core.fromnumeric import argmax
+from data_preparation import modifyData, changeToDummies, columns_to_boolean
 from random_forest import build_random_forest, parse_first_tree
 
 # Import external libraries
@@ -10,6 +11,8 @@ from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
 def main():
@@ -37,31 +40,33 @@ def main():
     # null_values = df.isnull().sum()
     # print(f"Number of null values per column:\n {null_values}")
 
-    # # Get correlation matrix
-    # correlation_matrix = df.corr()
-    # print(f"Correlation matrix:\n {correlation_matrix}")
+    # Get correlation matrix
+    correlation_matrix = df.corr()
+    print(f"Correlation matrix:\n {correlation_matrix}")
 
     # Get new data frame with modified data
     # For more info see data_manipulation.py
     df_new = modifyData(df)
 
+
     # Add age category to data frame
-    df_new = addAgeCategory(df_new)
     features = [f"{column}_Flag" for column in columns_to_boolean]
     features.extend(["Gender", "Age_Category", "Year_Of_Account_Creation_Category"])
 
-    for feature in features:
-        splot = sns.countplot(x=feature, data=df_new, palette="Set3")
-        for p in splot.patches:
-            splot.annotate(format(p.get_height(), '.2f'), (p.get_x() + p.get_width() / 2., p.get_height()), ha = 'center', va = 'center', xytext = (0, 10), textcoords = 'offset points')
+    # for feature in features:
+    #     splot = sns.countplot(x=feature, data=df_new, palette="Set3")
+    #     for p in splot.patches:
+    #         splot.annotate(format(p.get_height(), '.2f'), (p.get_x() + p.get_width() / 2., p.get_height()), ha = 'center', va = 'center', xytext = (0, 10), textcoords = 'offset points')
 
-        plt.xticks(rotation=45)
-        plt.show()
+    #     plt.xticks(rotation=45)
+    #     plt.show()
 
     # sns.pairplot(df_new[['Number_Of_Used_Services','Age']])
 
+    df_new = changeToDummies(df_new)
+
     # Create new data frame without New_Credit_Card_Flag column
-    df_x = df_new.drop(["New_Credit_Card_Flag", "Age_Category", "Year_Of_Account_Creation_Category"], axis=1)
+    df_x = df_new.drop(["New_Credit_Card_Flag"], axis=1)
 
     # Create new data frame with only New_Credit_Card_Flag column
     df_y = df_new["New_Credit_Card_Flag"]
@@ -89,21 +94,156 @@ def main():
     # neural_network_accuracy = accuracy_score(y_test, y_hat)
     # print(f"Accuracy score of neural network is {neural_network_accuracy}")
 
-    random_forest_model = build_random_forest(x_train, y_train)
+    # random_forest_model = build_random_forest(x_train, y_train)
 
-    y_hat = random_forest_model.predict(x_test)
+    # y_hat = random_forest_model.predict(x_test)
 
-    random_forest_acurracy = accuracy_score(y_test, y_hat)
-    print(f"Accuracy score random forest is {random_forest_acurracy}")
+    # random_forest_acurracy = accuracy_score(y_test, y_hat)
+    # print(f"Accuracy score random forest is {random_forest_acurracy}")
 
-    parse_first_tree(random_forest_model, x_train.columns)
+    # parse_first_tree(random_forest_model, x_train.columns)
 
-    # df_x = df_x[:30]
-    # df_y = df_y[:30]
+    number_of_clusters = range(2,20)
+    intertia = list()
+    kmeans_data = df_x.values
 
-    # cluster_model = KMeans(n_clusters=2)
-    # cluster_model.fit(df_x)
+    # for number in number_of_clusters:
+    #     cluster_test_model = KMeans(n_clusters=number, random_state=4)
+    #     cluster_test_model.fit(kmeans_data)
+    #     models_inertia = cluster_test_model.inertia_
+    #     intertia.append(models_inertia)
+    #     print(f"The inertia for: {number} clusters is {models_inertia}")
+
+    # fig, (ax1) = plt.subplots(1, figsize=(16,6))
+    # xx = np.arange(len(number_of_clusters))
+    # ax1.plot(xx, intertia)
+    # ax1.set_xticks(xx)
+    # ax1.set_xticklabels(number_of_clusters, rotation="vertical")
+    # plt.xlabel("Number of clusters")
+    # plt.ylabel("Inertia score")
+    # plt.title("Inertia Plot per k")
+    # plt.show()
+
     
+    cluster_model = KMeans(n_clusters=3, random_state=2)
+    cluster_model.fit(kmeans_data)
+    
+    print(f"The clusters are: {cluster_model.labels_}")
+
+    print(f"The inertia is: {cluster_model.inertia_}")
+
+    predictions = cluster_model.predict(kmeans_data)
+
+    unique, counts = np.unique(predictions, return_counts=True)
+    counts = counts.reshape(1,3)
+
+    cluster_names = ["Cluster 0","Cluster 1","Cluster 2"]
+
+    countscldf = pd.DataFrame(counts, columns = cluster_names)
+    print(countscldf)  
+
+    X = kmeans_data
+    y_num = predictions
+
+    pca = PCA(n_components=2, random_state = 453)
+    X_r = pca.fit(X).transform(X)
+
+    print(f'Explained variance ratio (first two components): {str(pca.explained_variance_ratio_)}')
+
+    plt.figure()
+    plt.figure(figsize=(12,8))
+    colors = ['navy', 'turquoise', 'darkorange']
+    lw = 2
+
+
+    for color, i, target_name in zip(colors, [0, 1, 2, 3, 4], cluster_names):
+        plt.scatter(X_r[y_num == i, 0], X_r[y_num == i, 1], color=color, alpha=.8, lw=lw,label=target_name)
+        
+    plt.legend(loc='best', shadow=False, scatterpoints=1)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.6)   
+    plt.title('PCA of 2 Items')
+    plt.show()
+
+    n_components = X.shape[1]
+    print(n_components)
+
+    # Running PCA with all components
+    pca = PCA(n_components=n_components, random_state = 453)
+    X_r = pca.fit(X).transform(X)
+
+
+    # Calculating the 95% Variance
+    total_variance = sum(pca.explained_variance_)
+    print("Total Variance in our dataset is: ", total_variance)
+    var_95 = total_variance * 0.95
+    print("The 95% variance we want to have is: ", var_95)
+    print("")
+
+    # Creating a df with the components and explained variance
+    a = zip(range(0,n_components), pca.explained_variance_)
+    a = pd.DataFrame(a, columns=["PCA Comp", "Explained Variance"])
+
+    # Trying to hit 95%
+    print("Variance explain with 1 n_compononets: ", sum(a["Explained Variance"][0:1]))
+    print("Variance explain with 2 n_compononets: ", sum(a["Explained Variance"][0:2]))
+    print("Variance explain with 3 n_compononets: ", sum(a["Explained Variance"][0:3]))
+    print("Variance explain with 4 n_compononets: ", sum(a["Explained Variance"][0:4]))
+    print("Variance explain with 5 n_compononets: ", sum(a["Explained Variance"][0:5]))
+    print("Variance explain with 10 n_compononets: ", sum(a["Explained Variance"][0:10]))
+    print("Variance explain with 15 n_compononets: ", sum(a["Explained Variance"][0:15]))
+    print("Variance explain with 20 n_compononets: ", sum(a["Explained Variance"][0:20]))
+    print("Variance explain with 25 n_compononets: ", sum(a["Explained Variance"][0:25]))
+    print("Variance explain with 30 n_compononets: ", sum(a["Explained Variance"][0:30]))
+    print("Variance explain with 32 n_compononets: ", sum(a["Explained Variance"][0:32]))
+
+    # Plotting the Data
+    plt.figure(1, figsize=(14, 8))
+    plt.plot(pca.explained_variance_ratio_, linewidth=2, c="r")
+    plt.xlabel('n_components')
+    plt.ylabel('explained_ratio_')
+
+    # Plotting line with 95% e.v.
+    plt.axvline(53,linestyle=':', label='n_components - 95% explained', c ="blue")
+    plt.legend(prop=dict(size=12))
+
+    # adding arrow
+    plt.annotate('2 eigenvectors used to explain 95% variance', xy=(2, pca.explained_variance_ratio_[2]), 
+                xytext=(58, pca.explained_variance_ratio_[10]),
+                arrowprops=dict(facecolor='blue', shrink=0.05))
+
+    plt.show()
+
+    pca = PCA(n_components=2, random_state = 453)
+    X_r = pca.fit(X).transform(X)
+
+    inertia = list()
+    inertia_difference = dict()
+
+    #running Kmeans
+
+    for f in number_of_clusters:
+        kmeans = KMeans(n_clusters=f, random_state=2)
+        kmeans = kmeans.fit(X_r)
+        u = kmeans.inertia_
+        inertia.append(u)
+        print("The innertia for :", f, "Clusters is:", u)
+        if len(inertia) >= 2:
+            difference = inertia[-2]-u
+            print(f"Difference in inertia between clusters: {difference}")
+            inertia_difference[f] = difference
+
+    print(inertia_difference)
+    # Creating the scree plot for Intertia - elbow method
+    fig, (ax1) = plt.subplots(1, figsize=(16,6))
+    xx = np.arange(len(number_of_clusters))
+    ax1.plot(xx, inertia)
+    ax1.set_xticks(xx)
+    ax1.set_xticklabels(number_of_clusters, rotation='vertical')
+    plt.xlabel('n_components Value')
+    plt.ylabel('Inertia Score')
+    plt.title("Inertia Plot per k")
+    plt.show()
+
     # color_scheme = np.array(["lightsalmon", "powderblue"])
 
     # plt.subplot(1,2,1)
